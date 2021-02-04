d7f39659919eaa52ad9cf086441ca1c8c7be7741
alexyunyun <1020626977@qq.com>
2021-02-04 15:07:57 +0800

    项目详情页添加项目文档内容框，添加项目增加页

--- blogvue/src/views/ProjectAdd.vue
+++ blogvue/src/views/ProjectAdd.vue
@@ -1,13 +1,123 @@
 <template>
-  <div>项目添加页</div>
+  <!--  项目添加页属于管理员功能，普通用户不可见-->
+  <div class="edit-container">
+    <el-container>
+      <el-header>
+        <div style="margin-top: 15px;" class="article-title">
+          <el-input placeholder="项目标题" v-model="title" class="input-with-select">
+          </el-input>
+        </div>
+      </el-header>
+      <el-main>
+        <div class="description">
+          <el-input
+              type="textarea"
+              autosize
+              placeholder="项目描述"
+              v-model="description">
+          </el-input>
+        </div>
+        <div class="edit-box">
+          <mavon-editor class="content-box"
+                        :scroll-style=prop.scrollStyle
+                        :placeholder="placeholder"></mavon-editor>
+        </div>
+
+      </el-main>
+      <el-main>
+        <div class="upload">
+          <el-upload
+              action="https://jsonplaceholder.typicode.com/posts/"
+              list-type="picture-card"
+              :on-preview="handlePictureCardPreview"
+              :on-remove="handleRemove">
+            <i class="el-icon-plus"></i>
+          </el-upload>
+          <el-dialog :visible.sync="dialogVisible">
+            <img width="100%" :src="dialogImageUrl" alt="">
+          </el-dialog>
+        </div>
+        <div class="link">
+          <el-input placeholder="项目链接" v-model="link"></el-input>
+        </div>
+        <el-row type="flex" justify="end" class="buttons">
+          <el-col :span="3">
+            <el-button type="danger" plain>返回</el-button>
+          </el-col>
+          <el-col :span="3">
+            <el-button type="primary" plain>保存</el-button>
+          </el-col>
+          <el-col :span="3">
+            <el-button type="success" plain>发布</el-button>
+          </el-col>
+        </el-row>
+      </el-main>
+
+    </el-container>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "ProjectAdd"
+  name: "BlogEdit",
+  data() {
+    return {
+      select: '',
+      title: '',
+      description:'',
+      link:'',
+      placeholder:'在此编写项目文档',
+      dialogImageUrl: '',
+      dialogVisible: false
+    }
+  },
+  methods: {
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    }
+  },
+  computed: {
+    prop() {
+      let settings = {
+        subfield: true,// 单双栏模式
+
+        defaultOpen: 'edit',//edit： 默认展示编辑区域 ， preview： 默认展示预览区域
+        editable: true,
+        toolbarsFlag: true,
+        scrollStyle: true
+      }
+      return settings
+    }
+  },
 }
 </script>
 
 <style scoped>
+.el-select {
+  width: 130px;
+}
+.input-with-select .el-input-group__prepend {
+  background-color: #fff;
+  width: 80%;
+}
 
+.content-box {
+  width: 100%;
+  border-radius: 5px;
+  height: max-content;
+  margin-top: 40px;
+}
+.buttons {
+  margin: 20px;
+}
+.upload {
+  background: white;
+  padding: 20px;
+  border-radius: 6px;
+  margin-bottom: 20px;
+}
 </style>
--- blogvue/src/views/ProjectDetails.vue
+++ blogvue/src/views/ProjectDetails.vue
@@ -14,7 +14,18 @@
           <span class="small-title">项目描述:</span>
           这是一个前后端分离的博客系统，主要应用的技术有springboot，vue，element-ui等
         </div>
+        <div class="readme">
+          <span class="small-title">项目文档:</span>
+          <mavon-editor class="content-box"
+                        :value="markdown"
+                        :subfield="prop.subfield"
+                        :defaultOpen="prop.defaultOpen"
+                        :toolbarsFlag="prop.toolbarsFlag"
+                        :editable="prop.editable"
+                        :scrollStyle="prop.scrollStyle">
 
+          </mavon-editor>
+        </div>
         <div class="images">
           <el-carousel :interval="4000" type="card" height="200px">
             <el-carousel-item v-for="item in images">
@@ -35,11 +46,12 @@
 
 <script>
 
-
+import markdown from '@/assets/SSM框架学习笔记.md'
 export default {
   name: "ProjectDetails",
   data() {
     return {
+      markdown,
       images: [
         'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
         'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
@@ -70,7 +82,19 @@ export default {
         }
       ]
     }
-  }
+  },
+  computed: {
+    prop() {
+      let settings = {
+        subfield: false,// 单双栏模式
+        defaultOpen: 'preview',//edit： 默认展示编辑区域 ， preview： 默认展示预览区域
+        editable: false,
+        toolbarsFlag: false,
+        scrollStyle: true
+      }
+      return settings
+    }
+  },
 
 }
 </script>
@@ -107,7 +131,8 @@ export default {
 
 .description {
   font-size: large;
-  margin: 20px;
+  margin-top: 20px;
+  margin-bottom: 35px;
 }
 .images {
   margin-top: 50px;
@@ -121,7 +146,7 @@ export default {
   font-size: x-small;
   font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
   color: #d1d1d1;
-  margin-right: 15px;
+  margin: 15px 15px 20px;
 }
 .link {
   font-size: large;
@@ -135,5 +160,11 @@ export default {
 }
 .link-inner:visited {
   text-decoration: none;
+}
+
+.content-box {
+  margin-top: 25px;
+  width: 100%;
+  border-radius: 5px;
 }
 </style>
